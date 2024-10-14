514e558714bbd78e237b22d362e33ea70fd52a0c
hvmegy <hamegy08lleo@gmail.com>
2024-10-14 21:49:43 +0700

    persistent.xml config

---
+++ src/main/java/ultils/Constant.java
@@ -0,0 +1,15 @@
+package ultils;
+
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class Constant {
+        public static final String SESSION_USERNAME = "username";
+        public static final String COOKIE_REMEMBER = "username";
+        public static final String REGISTER = "/view/web/register.jsp";
+        public static final String LOGIN = "/view/login.jsp";
+        public static final String FORGOT_PATH = "/view/forgot";
+        public static final String FORGOT_USER = "/view/forgot/user.jsp";
+        public static final String FORGOT_EMAIL = "/view/forgot/email.jsp";
+        public static final String DEFAULT_FILENAME = "D:\\University\\IMG";
+}
