d22380902d3b16f0e58bb48e96c7e57d8cf39339
hvmegy <hamegy08lleo@gmail.com>
2024-10-14 21:52:36 +0700

    dao

---
+++ src/main/java/dao/IVideoDao.java
@@ -0,0 +1,23 @@
+package dao;
+
+import entity.Video;
+
+import java.util.List;
+
+public interface IVideoDao {
+        void insert(Video video);
+
+        void update(Video video);
+
+        void delete(String videoid) throws Exception;
+
+        Video findById(String videoid);
+
+        List<Video> findAll();
+        List<Video> findByCategoryId(int categoryid);
+        List<Video> findByVideoTilte(String title);
+
+        List<Video> findAll(int page, int pagesize);
+
+        int count();
+}
